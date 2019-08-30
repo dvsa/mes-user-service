1b14ec602412481b0fb1d53633fa633d68986ab3
Ahmed Mobarak <ahmed.mobarak@hmcts.net>
2019-08-30 16:20:39 +0100

    Implemented findUser with unit tests

--- src/functions/getUser/application/service/FindUser.ts
+++ src/functions/getUser/application/service/FindUser.ts
@@ -1,36 +1,21 @@
-// import { getJournal } from '../../framework/aws/DynamoJournalRepository';
-// import { decompressJournal } from './journal-decompressor';
-// import * as logger from '../../../../common/application/utils/logger';
-// import { JournalNotFoundError } from '../../domain/errors/journal-not-found-error';
-// import { JournalDecompressionError } from '../../domain/errors/journal-decompression-error';
-// import { JournalRecord } from '../../domain/JournalRecord';
+import { getUserRecord } from '../../framework/aws/DynamoUserRepository';
+import { UserRecord } from '../../domain/UserRecord';
+import { UserNotFoundError } from '../../domain/user-not-found-error';
 
-// /**
-//  * Finds a journal with a specified staffNumber.
-//  * Throws a JournalNotFoundError if it the repo could not find one.
-//  * Throws a JournalDecompressionError if decompression fails
-//  * @param staffNumber the staff number of the journal to find
-//  */
-// export async function findJournal(
-//   staffNumber: string,
-//   modifiedSinceTimestamp: number | null,
-// ): Promise<ExaminerWorkSchedule | null> {
-//   const journalRecord = await getJournal(staffNumber);
-//   if (!journalRecord) {
-//     throw new JournalNotFoundError();
-//   }
-//   if (journalNotModifiedSince(journalRecord, modifiedSinceTimestamp)) {
-//     return null;
-//   }
+/**
+ * Finds a journal with a specified staffNumber.
+ * Throws a JournalNotFoundError if it the repo could not find one.
+ * Throws a JournalDecompressionError if decompression fails
+ * @param staffNumber the staff number of the journal to find
+ */
+export async function findUser(
+  staffNumber: string,
+): Promise<UserRecord> {
+  const userRecord = await getUserRecord(staffNumber);
+  console.log(userRecord);
+  if (!userRecord) {
+    throw new UserNotFoundError();
+  }
 
-//   try {
-//     return decompressJournal(journalRecord.journal);
-//   } catch (error) {
-//     logger.error(error);
-//     throw new JournalDecompressionError();
-//   }
-// }
-
-// const journalNotModifiedSince = (journalRecord: JournalRecord, modifiedSinceTimestamp: number | null): boolean => {
-//   return !!modifiedSinceTimestamp && journalRecord.lastUpdatedAt <= modifiedSinceTimestamp;
-// };
+  return userRecord;
+}
---
+++ src/functions/getUser/application/service/__tests__/FindUser.spec.data.ts
@@ -0,0 +1,13 @@
+import { UserRecord } from '../../../domain/UserRecord';
+
+export const userRecordFixture: UserRecord = {
+  role: 'DE',
+  staffNumber: '123456',
+  testPermissionPeriods: [
+    {
+      from: 'yesterday',
+      testCategory: 'B',
+      to: true,
+    }
+  ]
+}
