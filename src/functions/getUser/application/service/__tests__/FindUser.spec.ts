f97b6e390a968ce36a9826f6ec352ba3fd763846
Matthew Bell <mbell.dvsa@icloud.com>
2023-03-11 18:02:48 +0000

    Remove redundant code

--- src/functions/getUser/application/service/__tests__/FindUser.spec.ts
+++ src/functions/getUser/application/service/__tests__/FindUser.spec.ts
@@ -1,13 +1,7 @@
-import { Mock } from 'typemoq';
-import { ExaminerWorkSchedule } from '@dvsa/mes-journal-schema';
 import UserNotFoundError from '../../../domain/user-not-found-error';
 import userRecordFixture from './FindUser.spec.data';
 import findUser from '../FindUser';
 import * as DynamoUserRepository from '../../../framework/aws/DynamoUserRepository';
-
-const dummyWorkSchedule = Mock.ofType<ExaminerWorkSchedule>();
-dummyWorkSchedule.setup((x) => x.examiner?.staffNumber).returns(() => '00000000');
-// dummyWorkSchedule.setup((x) => x.then).returns(() => null);
 
 describe('FindUser', () => {
   describe('findUser', () => {
