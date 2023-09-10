3625f2a1f44079622d38ccccd6e48e42dc0bd41b
Matthew Bell <mbell.dvsa@icloud.com>
2023-09-10 10:27:03 +0100

    POC for integration tests with Serverless DynamoDB

---
+++ spec/integration/user.spec.ts
@@ -0,0 +1,22 @@
+describe('Integration test', () => {
+  const baseUrl = 'http://localhost:3000/dev/users';
+
+  it('should respond 200 for a user that exists', async () => {
+    try {
+      const response = await fetch(`${baseUrl}/1234567`);
+      expect(response.status).toEqual(200);
+      expect(await response.json()).toEqual({});
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  it('should respond 404 for a user that does not exist', async () => {
+    try {
+      const response = await fetch(`${baseUrl}/0000000`);
+      expect(response.status).toEqual(404);
+    } catch (err) {
+      throw err;
+    }
+  });
+});
