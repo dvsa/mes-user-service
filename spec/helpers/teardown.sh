dc04efa6c1cbaf9ad27b0799e7fedc753073b97a
Matthew Bell <mbell.dvsa@icloud.com>
2023-10-18 09:25:36 +0100

    Update int test

--- spec/helpers/teardown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-# Shut down any open serverless connections
-kill -9 "$(lsof -i:3000 | awk '{print $2}' | grep -v '^PID')"
-
-# Shut down any open serverless dynamodb local connections
-kill -9 "$(lsof -i:8000 | awk '{print $2}' | grep -v '^PID')"
