d5bb472df9a6b10615169ad78b724496b4b0046d
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2023-10-25 10:42:42 +0100

    AWS SDK v3 Upgrade (#72)
    * Update DynamoDB code to use new AWS sdkv3 provider
    
    * Update import path

--- src/functions/getUser/framework/aws/DynamoUserRepository.ts
+++ src/functions/getUser/framework/aws/DynamoUserRepository.ts
@@ -1,29 +1,37 @@
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
+import { fromIni } from '@aws-sdk/credential-providers';
 import { warn } from '@dvsa/mes-microservice-common/application/utils/logger';
 import { UserRecord } from '../../domain/UserRecord';
 
-const createDynamoClient = () => {
-  return process.env.IS_OFFLINE
-    ? new DynamoDB.DocumentClient({ endpoint: 'http://localhost:8000' })
-    : new DynamoDB.DocumentClient();
+const createDynamoClient = (): DynamoDBDocument => {
+  const opts = { region: 'eu-west-1' } as DynamoDBClientConfig;
+
+  if (process.env.USE_CREDENTIALS === 'true') {
+    warn('Using AWS credentials');
+    opts.credentials = fromIni();
+  } else if (process.env.IS_OFFLINE === 'true') {
+    warn('Using SLS offline');
+    opts.endpoint = process.env.DDB_OFFLINE_ENDPOINT;
+  }
+
+  return DynamoDBDocument.from(new DynamoDBClient(opts));
 };
 
 const ddb = createDynamoClient();
 const tableName = getUserTableName();
 
 export async function getUserRecord(staffNumber: string): Promise<UserRecord | null> {
-  const getUserResult = await ddb.get({
+  const response = await ddb.get({
     TableName: tableName,
-    Key: {
-      staffNumber,
-    },
-  }).promise();
+    Key: { staffNumber },
+  });
 
-  if (getUserResult.Item === undefined) {
+  if (response.Item === undefined) {
     return null;
   }
 
-  return getUserResult.Item as UserRecord;
+  return response.Item as UserRecord;
 }
 
 function getUserTableName(): string {
--- src/functions/getUser/framework/handler.ts
+++ src/functions/getUser/framework/handler.ts
@@ -1,39 +1,35 @@
-import { APIGatewayProxyEvent, APIGatewayProxyEventPathParameters, Context } from 'aws-lambda';
-import { bootstrapLogging, customMetric, error, warn } from '@dvsa/mes-microservice-common/application/utils/logger';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { bootstrapLogging, customMetric, error } from '@dvsa/mes-microservice-common/application/utils/logger';
 import createResponse from '../../../common/application/utils/createResponse';
 import { HttpStatus } from '../../../common/application/api/HttpStatus';
 import { findUser } from '../application/service/FindUser';
 import { UserNotFoundError } from '../domain/user-not-found-error';
 import { Metric } from '../../../common/application/metric/metric';
+import { getStaffNumber } from '../application/request-validator';
 
-export async function handler(event: APIGatewayProxyEvent, fnCtx: Context) {
+export async function handler(event: APIGatewayProxyEvent) {
   bootstrapLogging('user-service', event);
 
   const staffNumber = getStaffNumber(event.pathParameters);
   if (staffNumber === null) {
+    error('No staffNumber provided');
     return createResponse('No staffNumber provided', HttpStatus.BAD_REQUEST);
   }
 
   try {
     await findUser(staffNumber);
+
     customMetric(Metric.UserFound, 'User found in DynamoDB table using staff number provided');
+
     return createResponse({});
   } catch (err) {
     if (err instanceof UserNotFoundError) {
       customMetric(Metric.UserNotFound, 'User not found in DynamoDB table using staff number provided');
       return createResponse({}, HttpStatus.NOT_FOUND);
     }
-    error('Unknown error', err);
+
+    error((err instanceof Error) ? err.message : `Unknown error: ${err}`);
+
     return createResponse('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
   }
-}
-
-function getStaffNumber(pathParams: APIGatewayProxyEventPathParameters | null): string | null {
-  if (pathParams === null
-    || typeof pathParams.staffNumber !== 'string'
-    || pathParams.staffNumber.trim().length === 0) {
-    warn('No staffNumber path parameter found');
-    return null;
-  }
-  return pathParams.staffNumber;
 }
