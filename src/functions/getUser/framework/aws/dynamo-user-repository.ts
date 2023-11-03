6dd8d8cf31d41333ac2cdaebf8c9520e1bdee38c
Matthew Bell <mbell.dvsa@icloud.com>
2023-11-03 08:16:38 +0000

    Update yaml

--- src/functions/getUser/framework/aws/dynamo-user-repository.ts
+++ src/functions/getUser/framework/aws/dynamo-user-repository.ts
@@ -1,6 +1,6 @@
 import { GetCommand } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
-import { fromIni } from '@aws-sdk/credential-providers';
+import { fromEnv, fromIni } from '@aws-sdk/credential-providers';
 import { warn } from '@dvsa/mes-microservice-common/application/utils/logger';
 import { UserRecord } from '../../domain/UserRecord';
 
@@ -12,8 +12,7 @@ const createDynamoClient = () => {
     opts.credentials = fromIni();
   } else if (process.env.IS_OFFLINE === 'true') {
     warn('Using SLS offline');
-    warn('Offline EP', process.env.DDB_OFFLINE_ENDPOINT);
-    opts.credentials = { accessKeyId: 'accessKeyId', secretAccessKey: 'secretAccessKey' };
+    opts.credentials = fromEnv();
     opts.endpoint = process.env.DDB_OFFLINE_ENDPOINT;
   }
 
